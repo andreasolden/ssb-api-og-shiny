428d3ad5b56a842ba8071a23d5be9149842338c3
Andreas Olden <andreasolden@gmail.com>
2019-02-26 16:30:18 +0100

    Updated data wrangling
    Minor changes plus created a data frame with all the data for easy plotting

---
+++ ssb_api/app.R
@@ -0,0 +1,65 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+options(scipen=10000) #I hate scientific notation
+
+library("checkpoint") #Checkpoint assures us that we use the same package versions
+checkpoint("2019-01-21") #As they were at the date set in this function. i.e "2019-01-21"
+
+library(shiny) #This is the app part! 
+
+library(tidyverse) #For data wrangling and plotting
+library(here) # allows us to use relative file paths, see readme or simulations doc
+
+df_joined_data <- readRDS(here("data/processed", "df_joined_data.rds"))
+
+#
+#
+#
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
